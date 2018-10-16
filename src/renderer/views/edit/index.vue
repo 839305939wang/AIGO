7cf958c81c9c41f2c28dd5e73cd941a96a357b27
wang839305939 <wang839305939@outlook.com>
2018-10-16 22:46:14 +0800

    提交显示界面

--- src/renderer/views/edit/index.vue
+++ src/renderer/views/edit/index.vue
@@ -3,7 +3,40 @@
 */
 <template>
     <div class="editPage">
+        <div class="edit_bar">
 
+        </div>
+        <div class = "up">
+            <div class="up_left">
+                <!--图像显示区-->
+                <div class="video_view_content">
+
+                </div>
+                <!--图像缓冲区-->
+                <div class="video_image_list">
+                    
+                </div>
+            </div>
+            <div class="up_right">
+                <!--相机参数设置区-->
+                <div class="carmer_setting_content">
+                    <div class="carmer_setting_header">
+
+                    </div>
+                    <div class="carmer_setting_content">
+
+                    </div>
+                    <div class="carmer_setting_footer">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="down setting_content">
+            <!--参数设置区-->
+            <div class="setting_content_left"></div>
+            <div class="setting_content_right"></div>
+        </div>
     </div>
 </template>
 <script>
